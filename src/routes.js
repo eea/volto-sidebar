ea39bc4cdea0ff1114be24fa61f0689a68b930fa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-02 14:42:25 +0200

    Added Add route override that integrates the tabbed editor

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Edit } from 'volto-sidebar/Edit';
+import { Add } from 'volto-sidebar/Add';
 
 const routes = [
   {
+    path: '/add',
+    component: Add,
+  },
+  {
     path: '/edit',
     component: Edit,
+  },
+  {
+    path: '/**/add',
+    component: Add,
   },
   {
     path: '*/**/edit',
