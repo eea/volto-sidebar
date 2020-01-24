d29ef2a735b0c6b3fbf79686a4d812a37d8ce677
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-24 14:31:59 +0200

    Better behavior of mosaic view

--- src/ClonedBlocksView/ClonedBlocksView.jsx
+++ src/ClonedBlocksView/ClonedBlocksView.jsx
@@ -3,23 +3,19 @@ import MosaicView from 'volto-mosaic/components/theme/View';
 import DefaultView from '@plone/volto/components/theme/View/DefaultView';
 
 export default ({ content, ...props }) => {
-  return content?.cloned_blocks_layout?.mosaic_layout ? (
-    <MosaicView
-      content={{
-        ...content,
-        blocks: content.cloned_blocks,
-        blocks_layout: content.cloned_blocks_layout,
-      }}
-      {...props}
-    />
+  const useMosaic = content?.cloned_blocks_layout?.mosaic_layout ? true : false;
+  console.log('useMosaic', useMosaic, content, props);
+  return useMosaic ? (
+    <div id="mosaic-view">
+      <MosaicView {...props} />
+    </div>
   ) : (
-    <DefaultView
-      content={{
-        ...content,
-        blocks: content.cloned_blocks,
-        blocks_layout: content.cloned_blocks_layout,
-      }}
-      {...props}
-    />
+    <DefaultView content={content} {...props} />
   );
 };
+
+// content={{
+//   ...content,
+//   blocks: content.cloned_blocks,
+//   blocks_layout: content.cloned_blocks_layout,
+// }}
